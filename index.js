4c46f58ad30f57dbe049b61501d12dd241ce6071
unknown <sudiptoacharjee4@gmail.com>
2021-09-29 09:38:08 +0600

    get data by superagent

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    console.log(`Breed: ${data}`);
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+        console.log(res.body);
+    });
+
+})
